407761d728cdcb7fca8a25f5dba2b76da66a69de
areixyn <areixyn@gmail.com>
2026-08-21 23:26:35 +0800

    edited home page links

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import starlightSidebarTopics from 'starlight-sidebar-topics'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://areixyn.github.io',
-	base: '/the-library/en',
+	base: '/the-library',
 	integrations: [
 		starlight({
 			plugins: [
