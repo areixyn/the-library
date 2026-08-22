c2e8a563e6a6fc137a5ee2faa4b2c0ce2d11098e
areixyn <areixyn@gmail.com>
2026-08-22 18:19:00 +0800

    added images, fixed issue

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,6 @@
 
-import __ASTRO_IMAGE_IMPORT_4BcKf from "../../../assets/mets-welcome.PNG?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fen%2Findex.mdx";
-import __ASTRO_IMAGE_IMPORT_Z2flRyi from "../../../assets/mets-welcome.PNG?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fzh-tw%2Findex.mdx";
-export default new Map([["../../../assets/mets-welcome.PNG?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fen%2Findex.mdx", __ASTRO_IMAGE_IMPORT_4BcKf], ["../../../assets/mets-welcome.PNG?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fzh-tw%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z2flRyi]]);
+import __ASTRO_IMAGE_IMPORT_1KugOo from "../../../assets/mets-welc.PNG?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fen%2Findex.mdx";
+import __ASTRO_IMAGE_IMPORT_Z25DCxX from "../../../assets/mets-welc.PNG?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fzh-tw%2Findex.mdx";
+import __ASTRO_IMAGE_IMPORT_ZuIdE3 from "../../assets/mets-welc.PNG?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+export default new Map([["../../../assets/mets-welc.PNG?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fen%2Findex.mdx", __ASTRO_IMAGE_IMPORT_1KugOo], ["../../../assets/mets-welc.PNG?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fzh-tw%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z25DCxX], ["../../assets/mets-welc.PNG?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_ZuIdE3]]);
 		
